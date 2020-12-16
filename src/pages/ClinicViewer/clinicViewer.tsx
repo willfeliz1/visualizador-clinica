adc43d8100bae051563bc66ffae653b4ed20ed19
willfeliz1 <william_mfelizardo@hotmail.com>
2020-12-16 15:47:00 -0300

    feat: input pra selecionar arquivo xls

--- src/pages/ClinicViewer/clinicViewer.tsx
+++ src/pages/ClinicViewer/clinicViewer.tsx
@@ -1,16 +1,36 @@
 import { AppBar, Typography } from '@material-ui/core';
-import React from 'react';
-import { ToolBar } from './styles';
+import React, { useState } from 'react';
+// import * as XLSX from 'xlsx';
+import {
+  ToolBar,
+  Container,
+  GridContainer,
+  Main,
+  UploadContainer,
+} from './styles';
+import '../../assets/ListaDeClinicas.xlsx';
 
 const ClinicViewer: React.FC = () => {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
   return (
-    <>
-      <AppBar position="static">
+    <Container>
+      <AppBar position="absolute">
         <ToolBar>
           <Typography variant="h6">Visualizador de Clinicas</Typography>
         </ToolBar>
       </AppBar>
-    </>
+      <Main>
+        <GridContainer container spacing={1}>
+          {!uploadedFiles.length && (
+            <UploadContainer>
+              <Typography variant="h6">Selecione um arquivo</Typography>
+              <input type="file" />
+            </UploadContainer>
+          )}
+        </GridContainer>
+      </Main>
+    </Container>
   );
 };
 
