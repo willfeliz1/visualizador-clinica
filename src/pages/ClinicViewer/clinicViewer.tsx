1df0e342f48dbe9fca6f432ffe9bcc19b5945d55
willfeliz1 <william_mfelizardo@hotmail.com>
2020-12-17 04:51:39 -0300

    feat: modelo de layout para pagina listagem de Clinica com estilização

--- src/pages/ClinicViewer/clinicViewer.tsx
+++ src/pages/ClinicViewer/clinicViewer.tsx
@@ -1,17 +1,68 @@
-import { AppBar, Typography } from '@material-ui/core';
-import React, { useState } from 'react';
-// import * as XLSX from 'xlsx';
+import { AppBar, Chip, makeStyles, Typography } from '@material-ui/core';
+import { Done, Face, WhatsApp } from '@material-ui/icons';
+import React, { useCallback, useState } from 'react';
+import XLSX from 'xlsx';
 import {
   ToolBar,
   Container,
   GridContainer,
   Main,
   UploadContainer,
+  GridCard,
+  ClinicCard,
+  AvailableServices,
+  ChipService,
+  WhatsAppContainer,
+  ClinicInfo,
+  PhoneWhatsApp,
 } from './styles';
-import '../../assets/ListaDeClinicas.xlsx';
+
+// interface IUpload {
+//   nome: string;
+//   endereco: string;
+//   cep: string;
+//   email: string;
+//   whatsapp: string;
+//   servicosDisponiveis: string;
+// }
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  },
+});
 
 const ClinicViewer: React.FC = () => {
+  const classes = useStyles();
+
   const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const readxls = useCallback(
+    file => {
+      const fileReader = new FileReader();
+
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = e => {
+        const bufferArray = e.target!.result;
+
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        console.log(data);
+
+        setUploadedFiles(data);
+      };
+
+      console.log(uploadedFiles);
+    },
+    [uploadedFiles],
+  );
 
   return (
     <Container>
@@ -22,12 +73,102 @@ const ClinicViewer: React.FC = () => {
       </AppBar>
       <Main>
         <GridContainer container spacing={1}>
-          {!uploadedFiles.length && (
+          {/* {!uploadedFiles.length && (
             <UploadContainer>
               <Typography variant="h6">Selecione um arquivo</Typography>
-              <input type="file" />
+              <input
+                type="file"
+                onChange={e => {
+                  if (e.target.files !== null) {
+                    const file = e.target.files[0];
+                    readxls(file);
+                  }
+                }}
+              />
             </UploadContainer>
-          )}
+          )} */}
+          <GridCard item>
+            <ClinicCard>
+              <ClinicInfo>
+                <h2>EHS SOLUÇÕES INTELIGENTES</h2>
+                <div>
+                  <span>04602-002</span>
+                  <span>contato@ehsss.com.br</span>
+                </div>
+                <AvailableServices>
+                  <ChipService
+                    icon={<Face />}
+                    label="EXAMES CLINICOS"
+                    deleteIcon={<Done />}
+                  />
+                  <ChipService icon={<Face />} label="EXAMES COMPLEMENTARES" />
+                  <ChipService icon={<Face />} label="PPRA" color="primary" />
+                  <ChipService icon={<Face />} label="PCMSO" color="primary" />
+                </AvailableServices>
+              </ClinicInfo>
+
+              <WhatsAppContainer>
+                <div>
+                  <WhatsApp />
+                  <span>WhatsApp</span>
+                </div>
+
+                <PhoneWhatsApp>
+                  <h2>(11) 92477-9755</h2>
+                </PhoneWhatsApp>
+              </WhatsAppContainer>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+            <ClinicCard>
+              <div>
+                <h3>NOME DA CLINICA</h3>
+              </div>
+            </ClinicCard>
+          </GridCard>
         </GridContainer>
       </Main>
     </Container>
--- src/pages/ClinicViewer/styles.ts
+++ src/pages/ClinicViewer/styles.ts
@@ -1,7 +1,114 @@
-import { Toolbar } from '@material-ui/core';
+import { Button, Card, Chip, Grid, Toolbar } from '@material-ui/core';
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+`;
+
 export const ToolBar = styled(Toolbar)`
+  display: flex;
   justify-content: center;
   background-color: #9932cc;
+`;
+
+export const Main = styled.main`
+  flex-grow: 1;
+
+  padding: 75px 5px 5px;
+`;
+
+export const GridContainer = styled(Grid)`
+  height: 100vh;
+  justify-content: center;
+`;
+
+export const GridCard = styled(Grid)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  border-radius: 5px;
+`;
+
+export const ClinicCard = styled(Card)`
+  display: flex;
+  margin: 10px 0;
+  min-height: 150px;
+  min-width: 900px;
+  padding: 20px 0 0 30px;
+  border-radius: 10px;
+`;
+
+export const ClinicInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > div {
+    display: flex;
+
+    > span {
+      margin: 5px 30px 0 0;
+      color: #808080;
+    }
+  }
+`;
+
+export const AvailableServices = styled.div`
+  display: flex;
+  margin-top: 30px;
+
+  div + div {
+    margin-left: 10px;
+  }
+`;
+
+export const ChipService = styled(Chip)`
+  background-color: #3fb563;
+  color: #fff;
+  font-weight: bolder;
+
+  svg {
+    color: #fff;
+  }
+`;
+
+export const WhatsAppContainer = styled(Button)`
+  display: flex;
+  flex: 1;
+  width: 200px;
+  height: 110px;
+  margin: 5px 30px 0 25px;
+  background-color: #49b4d4;
+  border-radius: 15px;
+
+  span {
+    display: flex;
+    flex-direction: column;
+
+    div {
+      display: flex;
+      flex-direction: row;
+      color: #fff;
+
+      svg {
+        margin-right: 5px;
+      }
+    }
+  }
+`;
+
+export const PhoneWhatsApp = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  padding-top: 15px;
+  color: #fff;
+`;
+
+export const UploadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 100px;
 `;
